36f6cd77018193f8567938a7e520138ae4f6ba94
chanson20 <chanson20@bsmschool.org>
2019-02-06 11:52:44 -0600

    Updated direction

--- directionlibrary.py
+++ directionlibrary.py
@@ -1,1 +1,23 @@
 #This is functions for speed
+import RoboPiLib as RPL
+import setup
+
+def f():
+  RPL.servoWrite(1,1600)
+  RPL.servoWrite(0,1400)
+
+def l():
+  RPL.servoWrite(1,1600)
+  RPL.servoWrite(0,1500)
+
+def r():
+  RPL.servoWrite(1,1500)
+  RPL.servoWrite(0,1400)
+
+def b():
+  RPL.servoWrite(1,1400)
+  RPL.servoWrite(0,1600)
+
+def stop():
+  RPL.servoWrite(0,0)
+  RPL.servoWrite(1,0)
